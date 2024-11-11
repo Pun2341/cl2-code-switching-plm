66d880f5f1ca8d78ff08046ce6eb5c6302090712
Nick Chernogor <114106266+nickchernogor@users.noreply.github.com>
2024-11-11 18:40:10 -0500

    implement cls and mean pooling

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -1,12 +1,13 @@
 from transformers import BertModel, BertTokenizer, XLMRobertaTokenizer, XLMRobertaModel
 import torch
 
-def extract_layer_reps(model, sentences):
+def extract_layer_reps(model, cls_pooling, sentences):
     """
     Given: one of the following models:
         - mBERT ("bert-base-multilingual-cased"),
         - XLM-RoBERTa base ("xlm-roberta-base"),
         - XLM-RoBERTa large ("xlm-roberta-large"),
+    a boolean represnting whether the pooling type is CLS (true) or mean (false),
     and list of sentences.
 
     Returns: a tensor of shape (num_layers, num_sentences, sequence_length, hidden_size)
@@ -19,16 +20,28 @@ def extract_layer_reps(model, sentences):
         tokenizer = XLMRobertaTokenizer.from_pretrained(model.config._name_or_path)
     else:
         raise ValueError("unknown model")
-    inputs = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
     
+    inputs = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
+        
     outputs = model(**inputs, output_hidden_states=True)
     
-    return torch.stack(outputs.hidden_states) # (num_layers, num_sentences, sequence_length, hidden_size)
+    if cls_pooling: #cls
+
+        layers = torch.stack(outputs.hidden_states) #(num_layers, num_sentences, sequence_length, hidden_size)
+    
+        return layers[:, :, 0, :] #(num_layers, num_sentences, hidden_size)
+    
+    else: # mean pooling
+        attention_mask = inputs['attention_mask'][:, 1:].unsqueeze(-1)  # (num_sentences, sequence_length excluding first token, 1)
+        layers = []
+        for layer in outputs.hidden_states:
+            layers.append((layer[:, 1:, :] * attention_mask).sum(dim=1) / attention_mask.sum(dim=1))
+        return torch.stack(layers) #(num_layers, num_sentences, hidden_size)
 
 
 #example
 # initialize model to "bert-base-multilingual-cased", "xlm-roberta-base", or "xlm-roberta-large"
 # model = BertModel.from_pretrained("bert-base-multilingual-cased")
 # model = XLMRobertaModel.from_pretrained("xlm-roberta-base")
-# layers = extract_layer_reps(model, ["Hello how are you?", "Hola cómo estás?"])
+# layers = extract_layer_reps(model, False, ["Hello how are you?", "Hola cómo estás?"])
 # print(layers.shape)
