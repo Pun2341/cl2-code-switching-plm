d30963b032dded93fc4c0f857478688ccc9df5b8
Pun Chaixanien <sc2343@cornell.edu>
2024-11-11 23:36:57 -0500

    readied the script

--- classifier.py
+++ classifier.py
@@ -6,8 +6,10 @@ from sklearn.metrics import classification_report
 from extract_embeddings import extract_layer_reps
 import numpy as np
 from transformers import BertModel, BertTokenizer, XLMRobertaTokenizer, XLMRobertaModel
+import pickle
 
 # Load dataset
+print("Loading dataset...")
 df = pd.read_csv('classification_dataset.csv')
 
 # Assuming the dataset has two columns: 'sentences' and 'labels'
@@ -15,22 +17,41 @@ sentences = df['sentences'].values
 # embeddings = df['embedding'].values
 labels = df['labels'].values
 
-embeddings = extract_layer_reps(BertModel.from_pretrained("bert-base-multilingual-cased"), list(sentences))
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    embeddings, labels, test_size=0.2, random_state=42)
+# For each embedding type
+embedding_types = {'CLS': True, 'mean': False}
+for typ in embedding_types.keys():
+    print(f"Training SVM classifiers using {typ} embeddings...")
+    # Extract embeddings
+    print("Extracting embeddings...")
+    embeddings = extract_layer_reps(BertModel.from_pretrained(
+        "bert-base-multilingual-cased"), embedding_types[typ], list(sentences))
+    # For each layer
+    for i in range(len(embeddings)):
+        # Split the dataset into training and testing sets
+        print(f"Training SVM classifier for layer {i}...")
+        X_train, X_test, y_train, y_test = train_test_split(
+            embeddings[i], labels, test_size=0.2, random_state=42)
 
-# # Convert sentences to TF-IDF features
-# vectorizer = TfidfVectorizer()
-# X_train_tfidf = vectorizer.fit_transform(X_train)
-# X_test_tfidf = vectorizer.transform(X_test)
+        # Train an SVM classifier
+        svm_classifier = SVC(kernel='rbf')
+        svm_classifier.fit(X_train, y_train)
 
-# Train an SVM classifier
-svm_classifier = SVC(kernel='rbf')
-svm_classifier.fit(X_train, y_train)
+        # Predict on the test set
+        y_pred = svm_classifier.predict(X_test)
 
-# Predict on the test set
-y_pred = svm_classifier.predict(X_test)
+        # Print classification report
+        print(classification_report(y_test, y_pred))
 
-# Print classification report
-print(classification_report(y_test, y_pred))
+        # Save the model
+        with open(f'svm_classifier_layer_{typ}_{i}.pkl', 'wb') as f:
+            pickle.dump(svm_classifier, f)
+
+# Load Model
+# with open('svm_classifier.pkl', 'rb') as f:
+#     svm_classifier = pickle.load(f)
+
+# Predict on new data
+# new_data = SOME NEW DATA
+# new_embeddings = extract_layer_reps(BertModel.from_pretrained("bert-base-multilingual-cased"), list(new_data))
+# y_pred = svm_classifier.predict(new_embeddings)
+# print(classification_report(DATA LABELS, y_pred))
